a8cfc82d42462ed92719655537f2d57d4e78ac42
Kazuichi Takashiro <takashiro@qq.com>
2020-06-13 15:30:19 +0800

    Add update command

---
+++ src/cmd/update.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+
+import MessageBundle from '../base/MessageBundle';
+import searchSourceFiles from '../util/searchSourceFiles';
+import extractMessages from '../util/extractMessages';
+
+export const command = 'update';
+export const describe = 'Extract messages from source files and update message bundles.';
+
+exports.builder = {
+  banana: {
+    default: 'cool'
+  },
+  batman: {
+    default: 'sad'
+  }
+}
+
+interface Arguments {
+	locales: string;
+	sourceDir: string;
+	messageDir: string;
+}
+
+export async function handler(argv: Arguments) {
+  const sourceFiles = await searchSourceFiles(argv.sourceDir);
+	const messages = extractMessages(sourceFiles);
+	const locales = argv.locales.split(',');
+	for (const localeId of locales) {
+		const bundle = new MessageBundle(path.join(argv.messageDir, `${localeId}.json`));
+		await bundle.update(messages);
+	}
+}
