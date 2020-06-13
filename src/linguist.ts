d58a435236da461ef4b6f0e1125bf8562046ca3b
Kazuichi Takashiro <takashiro@qq.com>
2020-06-13 15:30:19 +0800

    Add update command

---
+++ src/linguist.ts
@@ -0,0 +1,27 @@
+import * as yargs from 'yargs';
+
+yargs
+.option('locales', {
+	describe: 'Define supported locales',
+	default: 'en-US,zh-CN',
+})
+.option('sourceDir', {
+	describe: 'The directory to search source files.',
+	default: 'src',
+})
+.option('messageDir', {
+	describe: 'The directory to save message bundles.',
+	default: 'message',
+})
+.option('distDir', {
+	describe: 'The directory to save minified JavaScript files of message bundles.',
+	default: 'dist/message',
+})
+.config()
+.default('config', '.linguist.json')
+.version()
+.commandDir('cmd')
+.recommendCommands()
+.demandCommand()
+.help()
+.argv;
