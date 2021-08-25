2075746193763e2411f42d188248958f5f388dbe
Kazuichi Takashiro <takashiro@qq.com>
2021-08-25 21:21:44 +0800

    Use *.js as configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true,
+	},
+	extends: [
+		'airbnb-base',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@typescript-eslint',
+	],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
+	rules: {
+		'class-methods-use-this': 'off',
+		'func-names': 'off',
+		'linebreak-style': 'off',
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				ts: 'never',
+				tsx: 'never',
+				js: 'never',
+				jsx: 'never',
+			},
+		],
+		indent: [
+			'error',
+			'tab',
+		],
+		'max-len': 'off',
+		'no-await-in-loop': 'off',
+		'no-continue': 'off',
+		'no-param-reassign': 'off',
+		'no-plusplus': 'off',
+		'no-restricted-syntax': [
+			'error',
+			'WithStatement',
+		],
+		'no-tabs': 'off',
+		'no-undef': 'off',
+		'prefer-arrow-callback': 'off',
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: [
+					'.ts',
+					'.js',
+				],
+			},
+		},
+	},
+};
